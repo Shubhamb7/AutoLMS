c596cbef61ba307b3b99a1f97127ab43ec89a3b9
Shubham <31106170+Shubhamb7@users.noreply.github.com>
2019-03-16 12:21:45 +0530

    Update Chlms.py

--- Chlms.py
+++ Chlms.py
@@ -1,5 +1,6 @@
 import os
 import time
+from tqdm import tqdm
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -8,11 +9,12 @@ options = Options()
 url_list = []
 subj_url_list = []
 subj_ids = []
+
+username = input('\n\nenter username : ')
+passwd = input('enter password : ')
+
 driver = webdriver.Chrome(options = options, executable_path=r"chromedriver.exe")
 og_win = driver.window_handles[0]
-
-username = input('enter username : ')
-passwd = input('enter password : ')
 driver.get("http://mydy.dypatil.edu/") 
 
 user = driver.find_element_by_xpath('//*[@id="username"]')
@@ -43,9 +45,9 @@ for i in range(0,6):
     subj_ids.append(subid)
     num+=1
 
-print('\nSUBJECT IDs : \n',subj_ids)
+print('\nin progess...\n')
 
-for i in range(0,6):
+for i in tqdm(range(0,6)):
     driver.get("http://mydy.dypatil.edu/rait/course/customview.php?id="+subj_ids[i])
     time.sleep(2)
     subj_links = driver.find_elements_by_class_name('pending')
@@ -53,11 +55,6 @@ for i in range(0,6):
     for subj_link in subj_links:
         subj_url = subj_link.get_attribute('href')
         subj_url_list.append(subj_url)
-       
-    print('\nSUBJECT['+subj_ids[i]+'] URLS : ')
-
-    for i in subj_url_list:
-        print(i)
 
     for i in subj_url_list:
         driver.execute_script('window.open("'+i+'","_blank")')
@@ -70,3 +67,15 @@ for i in range(0,6):
 
     driver.switch_to.window(og_win)
     subj_url_list = []
+
+setting = driver.find_element_by_class_name('usermenu')
+setting.click()
+
+links = driver.find_elements_by_tag_name('a')
+
+for link in links:
+    url = link.get_attribute('href')
+    url_list.append(url)
+
+driver.get(url_list[15])
+driver.close()
